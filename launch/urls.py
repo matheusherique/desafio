73c8c36cece68ed63ca3757b8811c24f1e3e7f73
Matheus Costa <matheus.bionicle@gmail.com>
2020-08-20 20:01:34 -0300

    Create latest get method from SpaceX API

--- launch/urls.py
+++ launch/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('api/launch/', views.LaunchListCreate.as_view()),
     path('next/', views.Rocket.next_launch, name='next'),
+    path('latest/', views.Rocket.latest_launch, name='latest'),
 ]
--- launch/views.py
+++ launch/views.py
@@ -37,3 +37,28 @@ class Rocket(object):
                 return JsonResponse(serializer.data, status=201)
             else:
                 return JsonResponse(serializer.errors, status=400)
+
+    def latest_launch(request):
+        if request.method == "GET":
+            r = requests.get('https://api.spacexdata.com/v3/launches/latest')
+            json = r.json()
+            launch = Launch()
+            launch.flight_number = int(json['flight_number'])
+            launch.launch_year = int(json['launch_year'])
+            launch.launch_date_utc = json['launch_date_utc']
+            launch.launch_date_local = json['launch_date_local']
+            launch.rocket_id = json['rocket']['rocket_id']
+            launch.rocket_name = json['rocket']['rocket_name']
+            launch.rocket_type = json['rocket']['rocket_type']
+            launch.land_success = False
+            launch.site_name = json['launch_site']['site_name_long']
+            launch.customer = json['rocket']['second_stage']['payloads'][0]['customers'][0]
+            launch.nationality = json['rocket']['second_stage']['payloads'][0]['nationality']
+            launch.launch_success = False
+
+            serializer = LaunchSerializer(data=launch.as_json())
+
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.errors, status=400)
