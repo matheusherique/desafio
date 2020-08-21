44c45d67c30b62ff3b4330fc170731631867cf7f
Matheus Costa <matheus.bionicle@gmail.com>
2020-08-21 08:47:12 -0300

    Add past launches from SpaceX API

--- launch/urls.py
+++ launch/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('next/', views.Rocket.next_launch, name='next'),
     path('latest/', views.Rocket.latest_launch, name='latest'),
     path('upcoming/', views.Rocket.upcoming_launches, name='upcoming'),
+    path('past/', views.Rocket.past_launches, name='past'),
 ]
--- launch/views.py
+++ launch/views.py
@@ -95,3 +95,34 @@ class Rocket(object):
                     __serializer.save()
 
             return JsonResponse(__launches, safe=False, status=201)
+
+    def past_launches(request):
+        __launches = []
+
+        if request.method == "GET":
+            __json = requests.get('https://api.spacexdata.com/v3/launches/past')
+            __json = __json.json()
+
+            for flight in __json:
+                __launch = Launch()
+                __launch.flight_number = int(flight['flight_number'])
+                __launch.launch_year = int(flight['launch_year'])
+                __launch.launch_date_utc = flight['launch_date_utc']
+                __launch.launch_date_local = flight['launch_date_local']
+                __launch.rocket_id = flight['rocket']['rocket_id']
+                __launch.rocket_name = flight['rocket']['rocket_name']
+                __launch.rocket_type = flight['rocket']['rocket_type']
+                __launch.land_success = False
+                __launch.site_name = flight['launch_site']['site_name_long']
+                __launch.customer = flight['rocket']['second_stage']['payloads'][0]['customers']
+                __launch.nationality = flight['rocket']['second_stage']['payloads'][0]['nationality']
+                __launch.manufacturer = flight['rocket']['second_stage']['payloads'][0]['manufacturer']
+                __launch.launch_success = False
+                __launch.video_link = flight['links']['video_link']
+
+                __launches.append(__launch.as_json())
+                __serializer = LaunchSerializer(data=__launch.as_json())
+                if __serializer.is_valid():
+                    __serializer.save()
+
+            return JsonResponse(__launches, safe=False, status=201)
