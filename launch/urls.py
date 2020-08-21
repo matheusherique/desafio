478e171b7ae414ab52f10e3f6e7b20a287638087
Matheus Costa <matheus.bionicle@gmail.com>
2020-08-21 08:38:44 -0300

    Add upcoming launches from SpaceX API

--- launch/urls.py
+++ launch/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('api/launch/', views.LaunchListCreate.as_view()),
     path('next/', views.Rocket.next_launch, name='next'),
     path('latest/', views.Rocket.latest_launch, name='latest'),
+    path('upcoming/', views.Rocket.upcoming_launches, name='upcoming'),
 ]
--- launch/views.py
+++ launch/views.py
@@ -14,55 +14,84 @@ class Rocket(object):
 
     def next_launch(request):
         if request.method == "GET":
-            json = requests.get('https://api.spacexdata.com/v3/launches/next')
-            json = json.json()
-            launch = Launch()
-            launch.flight_number = int(json['flight_number'])
-            launch.launch_year = int(json['launch_year'])
-            launch.launch_date_utc = json['launch_date_utc']
-            launch.launch_date_local = json['launch_date_local']
-            launch.rocket_id = json['rocket']['rocket_id']
-            launch.rocket_name = json['rocket']['rocket_name']
-            launch.rocket_type = json['rocket']['rocket_type']
-            launch.land_success = False
-            launch.site_name = json['launch_site']['site_name_long']
-            launch.customer = json['rocket']['second_stage']['payloads'][0]['customers'][0]
-            launch.nationality = json['rocket']['second_stage']['payloads'][0]['nationality']
-            launch.launch_success = False
-            launch.video_link = json['links']['video_link']
+            __json = requests.get('https://api.spacexdata.com/v3/launches/next')
+            __json = __json.json()
+            __launch = Launch()
+            __launch.flight_number = int(__json['flight_number'])
+            __launch.launch_year = int(__json['launch_year'])
+            __launch.launch_date_utc = __json['launch_date_utc']
+            __launch.launch_date_local = __json['launch_date_local']
+            __launch.rocket_id = __json['rocket']['rocket_id']
+            __launch.rocket_name = __json['rocket']['rocket_name']
+            __launch.rocket_type = __json['rocket']['rocket_type']
+            __launch.land_success = False
+            __launch.site_name = __json['launch_site']['site_name_long']
+            __launch.customer = __json['rocket']['second_stage']['payloads'][0]['customers'][0]
+            __launch.nationality = __json['rocket']['second_stage']['payloads'][0]['nationality']
+            __launch.launch_success = False
+            __launch.video_link = __json['links']['video_link']
 
-            print(json.keys())
+            __serializer = LaunchSerializer(data=__launch.as_json())
 
-            serializer = LaunchSerializer(data=launch.as_json())
-
-            if serializer.is_valid():
-                serializer.save()
-                return JsonResponse(serializer.data, status=201)
+            if __serializer.is_valid():
+                __serializer.save()
+                return JsonResponse(__serializer.data, status=201)
             else:
-                return JsonResponse(serializer.errors, status=400)
+                return JsonResponse(__serializer.errors, status=400)
 
     def latest_launch(request):
         if request.method == "GET":
-            r = requests.get('https://api.spacexdata.com/v3/launches/latest')
-            json = r.json()
-            launch = Launch()
-            launch.flight_number = int(json['flight_number'])
-            launch.launch_year = int(json['launch_year'])
-            launch.launch_date_utc = json['launch_date_utc']
-            launch.launch_date_local = json['launch_date_local']
-            launch.rocket_id = json['rocket']['rocket_id']
-            launch.rocket_name = json['rocket']['rocket_name']
-            launch.rocket_type = json['rocket']['rocket_type']
-            launch.land_success = False
-            launch.site_name = json['launch_site']['site_name_long']
-            launch.customer = json['rocket']['second_stage']['payloads'][0]['customers'][0]
-            launch.nationality = json['rocket']['second_stage']['payloads'][0]['nationality']
-            launch.launch_success = False
-            launch.video_link = json['links']['video_link']
+            __json = requests.get('https://api.spacexdata.com/v3/launches/latest')
+            __json = __json.json()
+            __launch = Launch()
+            __launch.flight_number = int(__json['flight_number'])
+            __launch.launch_year = int(__json['launch_year'])
+            __launch.launch_date_utc = __json['launch_date_utc']
+            __launch.launch_date_local = __json['launch_date_local']
+            __launch.rocket_id = __json['rocket']['rocket_id']
+            __launch.rocket_name = __json['rocket']['rocket_name']
+            __launch.rocket_type = __json['rocket']['rocket_type']
+            __launch.land_success = False
+            __launch.site_name = __json['launch_site']['site_name_long']
+            __launch.customer = __json['rocket']['second_stage']['payloads'][0]['customers'][0]
+            __launch.nationality = __json['rocket']['second_stage']['payloads'][0]['nationality']
+            __launch.launch_success = False
+            __launch.video_link = __json['links']['video_link']
 
-            serializer = LaunchSerializer(data=launch.as_json())
+            __serializer = LaunchSerializer(data=__launch.as_json())
 
-            if serializer.is_valid():
-                serializer.save()
-                return JsonResponse(serializer.data, status=201)
-            return JsonResponse(serializer.errors, status=400)
+            if __serializer.is_valid():
+                __serializer.save()
+                return JsonResponse(__serializer.data, status=201)
+            return JsonResponse(__serializer.errors, status=400)
+
+    def upcoming_launches(request):
+        __launches = []
+
+        if request.method == "GET":
+            __json = requests.get('https://api.spacexdata.com/v3/launches/upcoming')
+            __json = __json.json()
+
+            for flight in __json:
+                __launch = Launch()
+                __launch.flight_number = int(flight['flight_number'])
+                __launch.launch_year = int(flight['launch_year'])
+                __launch.launch_date_utc = flight['launch_date_utc']
+                __launch.launch_date_local = flight['launch_date_local']
+                __launch.rocket_id = flight['rocket']['rocket_id']
+                __launch.rocket_name = flight['rocket']['rocket_name']
+                __launch.rocket_type = flight['rocket']['rocket_type']
+                __launch.land_success = False
+                __launch.site_name = flight['launch_site']['site_name_long']
+                __launch.customer = flight['rocket']['second_stage']['payloads'][0]['customers']
+                __launch.nationality = flight['rocket']['second_stage']['payloads'][0]['nationality']
+                __launch.manufacturer = flight['rocket']['second_stage']['payloads'][0]['manufacturer']
+                __launch.launch_success = False
+                __launch.video_link = flight['links']['video_link']
+
+                __launches.append(__launch.as_json())
+                __serializer = LaunchSerializer(data=__launch.as_json())
+                if __serializer.is_valid():
+                    __serializer.save()
+
+            return JsonResponse(__launches, safe=False, status=201)
